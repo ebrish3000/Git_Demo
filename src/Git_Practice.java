b49610b3be83a4958b995dc35f97c70b1255bb7a
ebrish3000 <ebrish3000@gmail.com>
2020-08-01 22:02:15 -0700

    Add Array.jav

--- src/Git_Practice.java
+++ src/Git_Practice.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Git_Practice {
     public static void main(String[] args) {
 
         System.out.println("this is my first time to git practicing");
+
         ArrayList<Integer> list = new ArrayList<>();
+
+        String[] s = new String[5];
 
     }
 }

25914893731398dac78525d0314d13db6f64f2a0
ebrish3000 <ebrish3000@gmail.com>
2020-08-01 21:53:03 -0700

    Git practice.java

---
+++ src/Git_Practice.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Git_Practice {
+    public static void main(String[] args) {
+
+        System.out.println("this is my first time to git practicing");
+        ArrayList<Integer> list = new ArrayList<>();
+
+    }
+}
